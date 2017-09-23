9f632138264a7dc8b940fd56380fbdc8e9a9777c
silviodonato <silvio.donato@cern.ch>
2017-09-23 22:19:42 +0200

    working regression

--- regression.py
+++ regression.py
@@ -36,21 +36,21 @@ def main():
   # Load datasets.
   training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
       filename=test_training,
-      target_dtype=np.int,
+      target_dtype=np.float32,
       features_dtype=np.int)
   test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
       filename=test_evaluation,
-      target_dtype=np.int,
+      target_dtype=np.float32,
       features_dtype=np.int)
 
   # Specify that all features have real-value data
   feature_columns = [tf.feature_column.numeric_column("x", shape=[3])]
 
   # Build 3 layer DNN with 10, 20, 10 units respectively.
-  classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
+  regressor = tf.estimator.DNNRegressor(feature_columns=feature_columns,
                                           hidden_units=[10, 20, 10],
-                                          n_classes=30,
-                                          model_dir="/tmp/iris_model")
+#                                          model_dir="/tmp/iris_model"
+                                          )
   # Define the training inputs
   train_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={"x": np.array(training_set.data)},
@@ -59,7 +59,7 @@ def main():
       shuffle=True)
 
   # Train model.
-  classifier.train(input_fn=train_input_fn, steps=2000)
+  regressor.train(input_fn=train_input_fn, steps=2000)
 
   # Define the test inputs
   test_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -69,9 +69,12 @@ def main():
       shuffle=False)
 
   # Evaluate accuracy.
-  accuracy_score = classifier.evaluate(input_fn=test_input_fn)["accuracy"]
+  ev = regressor.evaluate(input_fn=test_input_fn, steps=1)
+  loss_score = ev["loss"]
+  print("Loss: {0:f}".format(loss_score))
 
-  print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
+
+#  print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
 
   # Classify two new flower samples.
 #28,2,0,1
@@ -83,19 +86,6 @@ def main():
 #28,5,19,0
 #28,9,18,2
 #28,16,4,0
-
-#28,2,0,1
-#28,11,6,0
-#28,1,8,3
-#28,7,10,1
-#28,15,12,-2
-#28,13,14,3
-#28,3,17,0
-#28,5,19,0
-#28,9,18,2
-#28,16,4,0
-
-
   
   new_samples = np.array([
     [28,2,0],
@@ -113,11 +103,13 @@ def main():
       num_epochs=1,
       shuffle=False)
 
-  predictions = list(classifier.predict(input_fn=predict_input_fn))
-  predicted_classes = [p["classes"] for p in predictions]
+  predictions = list(regressor.predict(input_fn=predict_input_fn))
+  predicted_classes = [p["predictions"] for p in predictions]
 
+  for i in range(len(new_samples)):
+    print(new_samples[i][0],new_samples[i][1],new_samples[i][2],'\t',predicted_classes[i])
   print(
-      "New Samples, Class Predictions:    {}\n"
+      "New Samples, Predictions:    {}\n"
       .format(predicted_classes))
 
 if __name__ == "__main__":
